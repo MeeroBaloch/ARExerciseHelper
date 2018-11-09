624be04e412cb1d64348f946e180503ee301a7f1
raunM <raun.mathur@gmail.com>
2018-11-08 23:48:10 -0500

    Scripts for video playback and event handler

---
+++ MainManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class MainManager : MonoBehaviour {
+
+    public VideoPlayer vid;
+
+    public GameObject playButton, pauseButton;
+
+    public GameObject mainCanvas;
+
+    // Use this for initialization
+    void Start () {
+        HideUI();
+    }
+
+    public void HideUI()
+    {
+        mainCanvas.SetActive(false);
+    }
+
+    public void DisplayUI()
+    {
+        mainCanvas.SetActive(true);
+    }
+
+    public void Play()
+    {
+        vid.Play();
+        playButton.SetActive(false);
+        pauseButton.SetActive(true);
+    }
+	
+    public void Pause()
+    {
+        vid.Pause();
+        pauseButton.SetActive(false);
+        playButton.SetActive(true);
+    }
+
+    public void Replay()
+    {
+        vid.Stop();
+        vid.Play();
+    }
+
+    public void Disable() {
+        pauseButton.SetActive(false);
+        playButton.SetActive(true);
+        mainCanvas.SetActive(false);
+        vid.Stop();
+    }
+}
